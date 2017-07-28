167831968bcab26613ca29d425db775eafb93e7d
ryanforstie <ryanforstie@gmail.com>
2017-07-27 17:24:06 -0700

    not sure what i changed

--- cart.js
+++ cart.js
@@ -18,12 +18,12 @@ function Form(product, productQuantity, userName, userStreet, userCity, userStat
 var nameGoesHere = JSON.parse(localStorage.storedUserInput);
 
 function displayUserInput() {
-document.getElementById('product').innerHTML = nameGoesHere[0].product;
+document.getElementById('products').innerHTML = nameGoesHere[0].product;
 document.getElementById('quantity').innerHTML = nameGoesHere[0].productQuantity;
 document.getElementById('name').innerHTML = nameGoesHere[0].userName;
 document.getElementById('street').innerHTML = nameGoesHere[0].userStreet;
 document.getElementById('city').innerHTML = nameGoesHere[0].userCity;
-ocument.getElementById('state').innerHTML = nameGoesHere[0].userState;
+document.getElementById('state').innerHTML = nameGoesHere[0].userState;
 document.getElementById('zipcode').innerHTML = nameGoesHere[0].userZip;
 document.getElementById('phonenumber').innerHTML = nameGoesHere[0].userPhoneNumber;
 document.getElementById('creditcard').innerHTML = nameGoesHere[0].userCreditCard;
